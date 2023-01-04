10f51822266200458379b3ed90241ed741524de2
ctoledo26 <coritoledo@gmail.com>
2023-01-04 10:07:43 -0500

    first commit

---
+++ src/main/java/projects/dao/DbConnection.java
@@ -0,0 +1,34 @@
+package projects.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import projects.exception.DbException;
+
+public class DbConnection {
+
+	
+	private static String HOST = "localhost";
+	private static String PASSWORD = "projectweek7";
+	private static int PORT = 3306;
+	private static String SCHEMA = "projects";
+	private static String USER = "projects";
+	
+	  public static Connection getConnection() {
+	        String url =
+	        		String.format("jdbc:mysql://%s:%d/%s?user=%s&password=%s&useSSL=false",HOST,PORT,SCHEMA,USER,PASSWORD);
+	       
+	        
+	        
+	        try {
+	        	  Connection connection = DriverManager.getConnection(url);
+	        	  System.out.println("Connection was succesfull");
+	        	  return connection;
+	        	} catch (SQLException e) {
+	        	  System.out.println("Error: Failed to establish connection to the database");
+	        	  throw new DbException(e);
+	        	}
+	    }
+	}
+
